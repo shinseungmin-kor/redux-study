91161175a82fc55a47e8aefda91605863af66dd9
shinseungmin-kor <shinseungmin10@gmail.com>
2021-09-22 00:39:48 +0900

    21.09.22

---
+++ src/components/DisplayNumberRoot.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import DisplayNumber from './DisplayNumber';
+
+
+export default class DisplayNumberRoot extends Component {
+    render(){
+      return (
+        <div>
+          <h1>Display Number Root</h1>
+          <DisplayNumber number={this.props.number} />
+        </div>
+      )
+    }
+  }
